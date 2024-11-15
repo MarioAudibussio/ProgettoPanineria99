ff83f60a68550b8da9856de13607e0cc1299f980
MarioAudibussio <mario.audibussio@edu.itspiemonte.it>
2024-11-15 11:20:01 +0100

    finocchio gio

---
+++ app/src/main/java/com/example/schedacibo/data/model/Post.kt
@@ -0,0 +1,10 @@
+package com.example.schedacibo.data.model
+
+data class Post(
+    val id: Int? = null,
+    val name: String? = null,
+    val prezzo: String? = null,
+    val tipologia: String? = null,
+    val descrizione: String? = null,
+    val immagine: String? = null
+)
