e49d9c0b0652d1eaf78a8f642ccd100da2eb1580
DavideArezzi <davide.arezzi@edu.itspiemonte.it>
2024-11-29 10:21:18 +0100

    ho aggiunto la mappa in InfoFragmentagment

---
+++ app/src/main/java/com/example/schedacibo/ProductAdapter.kt
@@ -0,0 +1,42 @@
+package com.example.schedacibo
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
+
+
+class ProdottoAdapter(private val prodottoList: List<Prodotto>) : RecyclerView.Adapter<ProdottoAdapter.ProdottoViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProdottoViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_prodotto1, parent, false)
+        return ProdottoViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ProdottoViewHolder, position: Int) {
+        val prodotto = prodottoList[position]
+
+        holder.nomeTextView.text = prodotto.nome
+        holder.descrizioneTextView.text = prodotto.descrizione
+        holder.prezzoTextView.text = prodotto.prezzo
+
+        // Carica l'immagine con Picasso
+        Picasso.get()
+            .load(prodotto.immagine)
+            .into(holder.immagineImageView)
+    }
+
+    override fun getItemCount(): Int {
+        return prodottoList.size
+    }
+
+    class ProdottoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val nomeTextView: TextView = itemView.findViewById(R.id.nameProduct)
+        val descrizioneTextView: TextView = itemView.findViewById(R.id.descriptionProduct)
+        val prezzoTextView: TextView = itemView.findViewById(R.id.priceProduct)
+        val immagineImageView: ImageView = itemView.findViewById(R.id.imageProduct)
+    }
+}
---
+++ app/src/main/java/com/example/schedacibo/data/api/RetrofitClient.kt
@@ -0,0 +1,15 @@
+package com.example.schedacibo.data.api
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClient {
+
+    private val BASE_URL = "https://jsonblob.com/"
+
+    var retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+        .create(ApiService::class.java)
+}
