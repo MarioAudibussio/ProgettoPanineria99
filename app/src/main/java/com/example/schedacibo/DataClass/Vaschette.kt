225e4193551d0d1ad514dde2539fcd1264b6788a
fidh3 <giorgio.fidelio@edu.itspiemonte.it>
2024-12-10 09:09:24 +0100

    primo commit SERIO di giorgionx

---
+++ app/src/main/java/com/example/schedacibo/DataClass/Vaschette.kt
@@ -0,0 +1,10 @@
+package com.example.schedacibo.DataClass
+
+class Vaschette (
+    val id: Int? = 0,
+    val immagine: String? = "",
+    val nome: String? = "",
+    val prezzo: String? = "",
+    val tipologia: String? = "",
+    val ingredienti: String? = "",
+)
