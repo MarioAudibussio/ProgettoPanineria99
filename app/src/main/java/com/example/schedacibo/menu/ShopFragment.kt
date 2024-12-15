5b6c412d6d41c8595845ab715fc05266647e24b2
MarioAudibussio <mario.audibussio@edu.itspiemonte.it>
2024-12-15 11:52:24 +0100

    Revert "Funziona tutto porco giuda"
    This reverts commit aa3cee4546eb12a1341b17be6b60e6ff9c377621.

--- app/src/main/java/com/example/schedacibo/menu/ShopFragment.kt
+++ app/src/main/java/com/example/schedacibo/menu/ShopFragment.kt
@@ -1,11 +1,9 @@
 package com.example.schedacibo.menu
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import com.example.schedacibo.CartManager
 import com.example.schedacibo.R
 import androidx.fragment.app.Fragment
@@ -16,7 +14,7 @@ import com.example.schedacibo.Adapter.CarrelloBibiteAdapter
 import com.example.schedacibo.Adapter.CarrelloPaniniAdapter
 import com.example.schedacibo.Adapter.CarrelloSpecialiAdapter
 import com.example.schedacibo.Adapter.CarrelloVaschetteAdapter
-import com.example.schedacibo.DataClass.Product
+
 class ShopFragment : Fragment() {
 
     private lateinit var recyclerView: RecyclerView
@@ -25,8 +23,6 @@ class ShopFragment : Fragment() {
     private lateinit var adapterpanini: CarrelloPaniniAdapter
     private lateinit var adapterspeciali: CarrelloSpecialiAdapter
     private lateinit var adaptervaschette: CarrelloVaschetteAdapter
-    private lateinit var costTextView: TextView
-    private lateinit var totalCostTextView: TextView
 
 
 
@@ -38,9 +34,6 @@ class ShopFragment : Fragment() {
         recyclerView = view.findViewById(R.id.recyclerView)
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
 
-        // Inizializza TextView per il costo totale
-        costTextView = view.findViewById(R.id.prezzo_costo)
-        totalCostTextView = view.findViewById(R.id.prezzo_costo_totale)
         // Ottieni gli elementi dal carrello
         val items = CartManager.getItems()
 
@@ -54,25 +47,6 @@ class ShopFragment : Fragment() {
 
         recyclerView.adapter = adapter
 
-        updateTotalCost(items)
-
         return view
-    }
-
-    private fun updateTotalCost(items: List<Product>) {
-        // Calcola il totale sommando i prezzi convertiti in Double
-        val cost = items.sumOf { item ->
-            // Rimuove caratteri non numerici (eccetto la virgola e il punto)
-            item.prezzo
-                ?.replace("€", "")  // Rimuove il simbolo €
-                ?.replace(",", ".") // Sostituisce la virgola con un punto
-                ?.trim()            // Elimina eventuali spazi
-                ?.toDoubleOrNull()  // Converte in Double
-                ?: 0.0              // Valore di default se fallisce
-        }
-        val totalCost = cost + 4.99
-        // Aggiorna la TextView con il totale
-        costTextView.text = "${String.format("%.2f", cost)}\€"
-        totalCostTextView.text = "${String.format("%.2f", totalCost)}\€"
     }
 }
